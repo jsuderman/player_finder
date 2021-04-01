8982dde16000b214b291631d1f1984cced2c1281
Justin Suderman <jsuderman1@gmail.com>
2021-04-01 15:09:37 -0700

    initial add and folder clean up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+        <h1>player finder app</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD_sovslGiOYTthMfCHEzmYhoNEoaQX9OM",
+    authDomain: "player-finder-88a2e.firebaseapp.com",
+    projectId: "player-finder-88a2e",
+    storageBucket: "player-finder-88a2e.appspot.com",
+    messagingSenderId: "606162220622",
+    appId: "1:606162220622:web:3bc4319c2597b7431bb51f",
+    measurementId: "G-C01SZS9GZ1"
+  };
