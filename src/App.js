a09de7d5a4b108696ef34d0ee394b10fd50ce71d
Justin Suderman <jsuderman1@gmail.com>
2021-04-02 09:15:03 -0700

    added navbar and mapped out app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomeScreen from "./components/HomeScreen/HomeScreen";
 
 function App() {
   return (
     <div className="app">
-        <h1>player finder app</h1>
+        <HomeScreen /> 
     </div>
   );
 }
